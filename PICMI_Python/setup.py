7299996c0dcba58a0b0f1ac47dffb1eaddb3c0d7
Remi Lehe <remi.lehe@normalesup.org>
2018-09-27 10:02:43 -0700

    Switch from distutils to setuptools

--- PICMI_Python/setup.py
+++ PICMI_Python/setup.py
@@ -2,23 +2,13 @@
 # To use:
 #       python setup.py install
 
-try:
-    import distutils
-    from distutils.core import setup
-except:
-    raise SystemExit('Distutils problem')
-
-try:
-    from distutils.command.build_py import build_py_2to3 as build_py
-except ImportError:
-    from distutils.command.build_py import build_py
+from setuptools import setup
 
 setup(name = 'picmistandard',
-      version = '1.0.0',
+      version = '0.0.1',
       description = 'Python base classes for PICMI standard',
       platforms = 'any',
       packages = ['picmistandard'],
       package_dir = {'picmistandard': '.'},
-      cmdclass = {'build_py':build_py}
       )
 

534985343568aff29dd78ef1f4b11670dce94a98
Remi Lehe <remi.lehe@normalesup.org>
2020-12-14 10:25:10 -0800

    Update version number

--- PICMI_Python/setup.py
+++ PICMI_Python/setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name = 'picmistandard',
-      version = '0.0.12',
+      version = '0.0.13',
       description = 'Python base classes for PICMI standard',
       platforms = 'any',
       packages = ['picmistandard'],

57187f2da8a320a05bdef1b231ef59ae704189fd
Remi Lehe <remi.lehe@normalesup.org>
2021-12-08 16:09:33 -0800

    Update version number

--- PICMI_Python/setup.py
+++ PICMI_Python/setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
     install_requires = [ line.strip('\n') for line in f.readlines() ]
 
 setup(name = 'picmistandard',
-      version = '0.0.17',
+      version = '0.0.18',
       description = 'Python base classes for PICMI standard',
       platforms = 'any',
       packages = ['picmistandard'],

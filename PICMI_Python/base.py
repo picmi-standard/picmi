dabb1a4e68bad2e33e8e7a7f1143b846c6345b49
David Grote <grote1@llnl.gov>
2022-03-14 14:20:53 -0700

    Update PICMI_Python/base.py
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- PICMI_Python/base.py
+++ PICMI_Python/base.py
@@ -109,16 +109,20 @@ class _ClassWithInit(object):
 
     def _check_deprecated_argument(self, arg_name, extra_info=None, raise_error=False):
         """Raise a warning or exception if a deprecated argument was specified by the user
-         - arg_name: The name of the deprecated argument (string)
-         - extra_info: Extra information to include in the warning/error message (string)
-         - raise_error: If False (the default), raise a warning. If true, raise an exception
-                        (which interrupts the code).
+
+        Implementation note: This should be called within PICMI in the "__init__" method of classes
+        for each deprecated argument. This assumes that the argument is still included in the
+        argument list of __init__ as a transition until it is removed.
+        For example, if the 'density_scale' argument of Species was to be deprecated:
+            self._check_deprecated_argument(
+                'density_scale',
+                extra_info='This argument is no longer needed')
+
+        - arg_name: The name of the deprecated argument (string)
+        - extra_info: Extra information to include in the warning/error message (string)
+        - raise_error: If False (the default), raise a warning. If true, raise an exception
+                       (which interrupts the code).
         """
-        # --- Implementation note: This should be called within PICMI in the "__init__" method of classes
-        # --- for each deprecated argument. This assumes that the argument is still included in the
-        # --- argument list of __init__ as a transition until it is removed.
-        # --- For example, if the 'density_scale' argument of Species was to be deprecated:
-        # ---     self._check_deprecated_argument('density_scale', extra_info='This argument is no longer needed')
 
         # This compares the value of the parameter with the dault value in the __init__ method.
         # If they differ, this means that the user supplied a value, so a warning or error is raised.
